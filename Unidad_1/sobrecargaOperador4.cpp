0ced95eee0bbdb34d712519e92325906a802d539
NEVargas <nahuvargas24@gmail.com>
2026-04-19 18:14:51 -0300

    change: elimino espacios innecesarios en ejemplo

--- Unidad_1/sobrecargaOperador4.cpp
+++ Unidad_1/sobrecargaOperador4.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-
 using namespace std;
 
 struct Vector{
     int x, y;
 };
-
 // Sobrecarga de operador con funcion externa
 Vector operator+(Vector &a, Vector &b){
     Vector resultado;
@@ -14,20 +12,18 @@ Vector operator+(Vector &a, Vector &b){
     resultado.y = a.y + b.y;
     return resultado;
 }
-
 ostream& operator<<(ostream &miCout, const Vector &rta){
     miCout <<"(" << rta.x << ", " << rta.y << ")"<< endl;
     return miCout;
 }
-
 int main(){
     Vector a={2,3};
     Vector b={4,5};
 
     Vector c = a + b;
-    
     cout << "Resultado de suma: " << c << endl;
 
     return 0;
 }
+
 
