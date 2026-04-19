2c69f0b91acd06ef2b72296e9f4f6c9ca140a868
NEVargas <nahuvargas24@gmail.com>
2026-04-19 18:00:59 -0300

    change: macro en return de sobrecargafunciones2

--- Unidad_1/sobrecargaFunciones2.cpp
+++ Unidad_1/sobrecargaFunciones2.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#define ERROR -1
+
 using namespace std;
 
 // Prototipo de funciones
@@ -9,7 +11,7 @@ int multiplicar(int, int, int, int);
 int main() {
     int value = multiplicar(10,2,4,6);
 
-    if(value == -1){
+    if(value == ERROR){
         return 0;
     }
     else{
@@ -30,7 +32,7 @@ int multiplicar(int a, int b, int c, int d){
     // Validacion previa a realizar operacion
     if(a<0 || b<0 || c<0 || d<0){
         cout << "[ERROR] Valor no valido" << endl;
-        return -1;
+        return ERROR;
     }
     else{
         return a*b*c*d;  
