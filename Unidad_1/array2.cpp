ac31332d0a82473d278dac4ec2ae56f11edfdc46
NEVargas <nahuvargas24@gmail.com>
2026-04-19 18:02:22 -0300

    dicart change: ejemplo array2 - se borro parte de codigo para mostrar uso de git

--- Unidad_1/array2.cpp
+++ Unidad_1/array2.cpp
@@ -7,7 +7,10 @@ int main()
     int numeros[5]={14, 25 ,47 ,58, 17};
 
     // Mostrar primer y ultimo elemento
+    cout<< "Primer numero: "<< numeros[0]<<endl;
+    cout<< "Ultimo numero: "<< numeros[4]<<endl;
 
+    cout<< "------------" << endl;
     // Mostrar por terminal orden descendente
     for(int i=4; i>-1; i--){
         cout<< "Numero " << i <<": "<<numeros[i] << endl;
