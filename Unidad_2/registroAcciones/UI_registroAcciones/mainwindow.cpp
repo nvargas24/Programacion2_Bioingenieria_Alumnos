954e74d2c2a15ca302aa2f73a0ec9921aff2a876
NEVargas <nahuvargas24@gmail.com>
2026-06-03 21:15:40 -0300

    add: UI de acciones

--- Unidad_2/registroAcciones/UI_registroAcciones/mainwindow.cpp
+++ Unidad_2/registroAcciones/UI_registroAcciones/mainwindow.cpp
@@ -12,3 +12,38 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_btnIngresar_clicked()
+{
+    acciones.push(ui->inAcciones->text());
+    qDebug() << "[INFO] Se carga a stack: " << acciones.top();
+}
+
+void MainWindow::on_btnTodo_clicked()
+{
+    QStringList misAcciones;
+
+    ui->labelUltima->setText(acciones.top());
+    while(! acciones.empty()){
+        qDebug() << "Ultima accion: " << acciones.top();
+        misAcciones.prepend(acciones.top());
+        acciones.pop();
+    }
+
+    qDebug() << "[INFO] Stack vacio ";
+    ui->listAcciones->addItems(misAcciones);
+}
+
+
+void MainWindow::on_btnUltimo_clicked()
+{
+    ui->labelUltima->setText(acciones.top());
+    qDebug() << "[INFO] Mostrar ultimo elemento de stack ";
+}
+
+
+void MainWindow::on_inAcciones_returnPressed()
+{
+    MainWindow::on_btnIngresar_clicked();
+}
+
