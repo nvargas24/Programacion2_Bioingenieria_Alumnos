29527ea62484282481843c50b6d4ca3f950c455a
Nahuel Vargas <68164200+nvargas24@users.noreply.github.com>
2026-06-24 15:09:42 -0300

    add: ejemplo capas de abstraccion

---
+++ Unidad_3/Oximetro.c
@@ -0,0 +1,71 @@
+#include <stdint.h>
+
+/* --- CAPA DE REGISTROS ---*/
+#define PERIPHERAL_BASE_ADDR   0x40012400
+#define SENSOR_DATA_REG_OFFSET 0x0C
+#define SENSOR_HARDWARE ((Medical_Sensor_Ports*)(PERIPHERAL_BASE_ADDR + SENSOR_DATA_REG_OFFSET))
+
+typedef struct {
+    volatile uint16_t RED_CHANNEL;  // Registro físico canal Rojo
+    volatile uint16_t IR_CHANNEL;   // Registro físico canal Infrarrojo
+} Medical_Sensor_Ports;
+
+/* --- CAPA HAL --- */
+uint16_t HAL_Read_Optical_Buffer(uint8_t channel) {
+    if (channel == 0) {
+        return SENSOR_HARDWARE->RED_CHANNEL; // Acceso directo a registro físico
+    } else {
+        return SENSOR_HARDWARE->IR_CHANNEL;
+    }
+}
+
+/* --- CAPA DE DRIVERS --- */ 
+typedef struct {
+    uint16_t red;
+    uint16_t ir;
+} PulseOx_Data_t;
+
+PulseOx_Data_t Driver_PulseOx_GetSamples(void) {
+    PulseOx_Data_t samples;
+    // El driver sabe que el oxímetro necesita combinar ambas lecturas ópticas
+    samples.red = HAL_Read_Optical_Buffer(0);
+    samples.ir  = HAL_Read_Optical_Buffer(1);
+    return samples;
+}
+
+/* --- CAPA DE SERVICIO --- */
+float Servicio_Calcular_SpO2(PulseOx_Data_t data) {
+    if (data.ir == 0) return 0.0f;
+    
+    // Algoritmo matemático para aproximar la saturación de oxígeno (R Ratio)
+    float r_ratio = ((float)data.red) / ((float)data.ir);
+    float spo2 = 110.0f - (25.0f * r_ratio); // Ecuación de calibración estándar
+    
+    if (spo2 > 100.0f) spo2 = 100.0f;
+    return spo2;
+}
+
+/* --- CAPA DE APLICACIÓN ---*/
+void Disparar_Alarma_Prioridad_Alta(void) {
+    // Activar buzzer
+}
+
+int main(void) {
+    // Inicialización
+    PulseOx_Data_t medicion_actual;
+    float saturacion_oxigeno;
+
+    while (1) {
+        // Se adquiere los datos estructurados
+        medicion_actual = Driver_PulseOx_GetSamples();
+        
+        // Se procesa los datos
+        saturacion_oxigeno = Servicio_Calcular_SpO2(medicion_actual);
+        
+        // Criterio de funcionamiento del sistema
+        if (saturacion_oxigeno < 90.0f) {
+            Disparar_Alarma_Prioridad_Alta();
+        }
+    }
+    return 0;
+}
