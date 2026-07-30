391fc33f1e310d7b229b485c1d83d9fdb1fb93ed
NEVargas <nahuvargas24@gmail.com>
2026-07-30 00:52:35 -0300

    add: ejemplo RGB

---
+++ Unidad_4/ledRGB/aplicacion/app_config.h
@@ -0,0 +1,12 @@
+#ifndef APP_CONFIG_H_
+#define APP_CONFIG_H_
+
+#define COLOR_ROTATION_PERIOD_MS 500
+
+typedef enum {
+    STATE_COLOR_RED,
+    STATE_COLOR_GREEN,
+    STATE_COLOR_BLUE
+} app_state_t;
+
+#endif
