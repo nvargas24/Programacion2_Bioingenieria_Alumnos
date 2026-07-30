715c7ce8ead103368d021ce2c3776c0001663d8a
NEVargas <nahuvargas24@gmail.com>
2026-07-29 23:51:59 -0300

    add: version2 de ejemplo blinky

---
+++ Unidad_4/led_blinky_V2/aplicacion/app_config.h
@@ -0,0 +1,12 @@
+#ifndef APP_CONFIG_H_
+#define APP_CONFIG_H_
+
+#define BLINK_PERIOD_MS 200
+#define LED_PORT LED_BLUE_PORT 
+#define LED_PIN LED_BLUE_PIN
+typedef enum {
+    LED_OFF,
+    LED_ON
+} app_state_t;
+
+#endif
---
+++ Unidad_4/led_blinky_V2/aplicacion/main.c
@@ -0,0 +1,41 @@
+#include "hardware_init.h"
+#include "sys_time.h"
+#include "driver_led.h"
+#include "app_config.h"
+
+int main(){
+    uint32_t lastToggleTick = 0;
+    app_state_t currentState = LED_OFF;
+
+    BSP_Hardware_Init();
+
+    Driver_LED_Init(LED_PORT, LED_PIN);
+    Driver_LED_Off(LED_PORT, LED_PIN);
+
+    lastToggleTick = sysTime_getTicks();
+
+    while(1){
+        switch (currentState)
+        {
+        case LED_OFF:
+            if(sysTime_hasElapsed(lastToggleTick, BLINK_PERIOD_MS)){
+                Driver_LED_On(LED_PORT, LED_PIN);
+                lastToggleTick = sysTime_getTicks();
+                currentState = LED_ON;
+            }
+            break;
+        case LED_ON:
+            if(sysTime_hasElapsed(lastToggleTick, BLINK_PERIOD_MS)){
+                Driver_LED_Off(LED_PORT, LED_PIN);
+                lastToggleTick = sysTime_getTicks();
+                currentState = LED_OFF;
+            }
+            break;
+        
+        default:
+            currentState = LED_OFF;
+            break;
+        }
+    }
+
+}
