b53da440e607faa6c1ac39cbe856560347130d7e
Nahuel Vargas <68164200+nvargas24@users.noreply.github.com>
2026-08-19 21:47:53 -0300

    change: limpieza codigo biseccion

--- Unidad_12/test_biseccion.cpp
+++ Unidad_12/test_biseccion.cpp
@@ -3,9 +3,6 @@
 
 using namespace std;
 
-// f(x) = x^3 - 6*x^2 + 11*x - 6
-#define F_SENSOR(x)             (((x) * (x) * (x)) - (6.0f * (x) * (x)) + (11.0f * (x)) - 6.0f)
-
 #define RANGO_INICIAL_A         2.2f
 #define RANGO_INICIAL_B         3.6f
 #define RAIZ_TEORICA            3.0f   // Raíz exacta para evaluar el Error% Verdadero
@@ -13,8 +10,9 @@ using namespace std;
 #define ITERACIONES_SOLICITADAS 10     // Para la Prueba 1
 #define ERROR_TOLERADO_OBJETIVO 0.05f  // Para la Prueba 2 (Detenerse al alcanzar <= 0.05% de error verdadero)
 
-float evaluar_sensor(float x) {
-    return F_SENSOR(x);
+float funcion_test(float x) { 
+    // f(x) = x^3 - 6*x^2 + 11*x - 6
+    return (x * x * x) - (6.0f * x * x) + (11.0f * x) - 6.0f; 
 }
 
 int main() {
@@ -29,38 +27,25 @@ int main() {
 
     cout << ">>> PRUEBA 1: Biseccion por Iteraciones Fijas :" << ITERACIONES_SOLICITADAS << endl;
     
-    estado = calcular_biseccion(evaluar_sensor, 
+    estado = calcular_biseccion(funcion_test, 
                                 RANGO_INICIAL_A, 
                                 RANGO_INICIAL_B, 
                                 ITERACIONES_SOLICITADAS, 
                                 RAIZ_TEORICA, 
                                 &resultado_biseccion);
 
-    if (estado == 0) {
-        cout << "[MAIN - ITERACIONES]: Raiz final calculada: " << resultado_biseccion << endl << endl;
-    } else {
-        cout << "[MAIN - SENSOR]: Operacion cancelada en Prueba 1. Codigo: " << estado << endl << endl;
-    }
-
-    resultado_biseccion = 0.0f; // Limpieza de variable para la siguiente prueba
-
     // ------------------------------------------------------------------------
     // PRUEBA 2: Método de bisección por tolerancia de error verdadero
     // ------------------------------------------------------------------------
-    cout << ">>> PRUEBA 2: Biseccion por Tolerancia de Error Verdadero (Detener en <= " << ERROR_TOLERADO_OBJETIVO << "%) <<<\n";
+    resultado_biseccion = 0.0f; // Limpieza de variable para la siguiente prueba    
+    cout << ">>> PRUEBA 2: Biseccion por Tolerancia de Error Verdadero (Detener en <= " << ERROR_TOLERADO_OBJETIVO << "%)" << endl;
     
-    estado = calcular_biseccion_por_error(evaluar_sensor, 
+    estado = calcular_biseccion_por_error(funcion_test, 
                                           RANGO_INICIAL_A, 
                                           RANGO_INICIAL_B, 
                                           ERROR_TOLERADO_OBJETIVO, 
                                           RAIZ_TEORICA, 
                                           &resultado_biseccion);
-
-    if (estado == 0) {
-        cout << "[MAIN - ERROR]: Raiz final calculada: " << resultado_biseccion << " unidades.\n";
-    } else {
-        cout << "[MAIN - SENSOR]: Operacion cancelada en Prueba 2. Codigo: " << estado << "\n";
-    }
 
     return 0;
 }
