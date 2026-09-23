48cc5c37a073b00ae7f2943622f486ab5736309a
Nahuel Vargas <68164200+nvargas24@users.noreply.github.com>
2026-09-22 23:59:43 -0300

    delete: archivos uart que no corresponde a ejemplo blinky

--- Unidad_5/led_blinky_5capas/registros/board.c
+++ Unidad_5/led_blinky_5capas/registros/board.c
@@ -9,7 +9,6 @@
 #include "fsl_common.h"
 #include "clock_config.h"
 #include "board.h"
-#include "fsl_debug_console.h"
 
 /*******************************************************************************
  * Variables
